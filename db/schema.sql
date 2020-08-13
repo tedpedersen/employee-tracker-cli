b7620219cf4f173052a7b15ffd3c6daf5fd635a9
Ted Pedersen <tedpedersen@me.com>
2020-08-13 14:17:46 -0700

    init files

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE department (
+  id INTEGER PRIMARY KEY,
+  dept_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE role (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  salary DECIMAL,
+  dept_id INTEGER UNSIGNED,
+  CONSTRAINT fk_dept FOREIGN KEY (dept_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER UNSIGNED,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  manager_id INTEGER
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO dept (id, name)
+VALUES 
+(1, “Sales”),
+(2, “Engineering”),
+(3, “Finace”),
+(4, “Legal”);
